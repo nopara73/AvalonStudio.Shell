bbaf03072e072fb4456181c2847a0f2cad5d2955
nopara73 <adam.ficsor73@gmail.com>
2018-11-12 18:09:38 +0700

    [skip ci] Use "Restore Down" instead of "Restore"

--- src/AvalonStudio.Shell/Controls/MetroWindow.cs
+++ src/AvalonStudio.Shell/Controls/MetroWindow.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 
 namespace AvalonStudio.Shell.Controls
 {
-    public class MetroWindow : Window, IStyleable
-    {
+	public class MetroWindow : Window, IStyleable
+	{
 		public MetroWindow()
 		{
 			if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
@@ -21,120 +21,120 @@ namespace AvalonStudio.Shell.Controls
 		}
 
 		public static readonly AvaloniaProperty<Control> TitleBarContentProperty =
-            AvaloniaProperty.Register<MetroWindow, Control>(nameof(TitleBarContent));
+			AvaloniaProperty.Register<MetroWindow, Control>(nameof(TitleBarContent));
 
-        private Grid _bottomHorizontalGrip;
-        private Grid _bottomLeftGrip;
-        private Grid _bottomRightGrip;
-        private Button _closeButton;
-        private Image _icon;
-        private Grid _leftVerticalGrip;
-        private Button _minimiseButton;
+		private Grid _bottomHorizontalGrip;
+		private Grid _bottomLeftGrip;
+		private Grid _bottomRightGrip;
+		private Button _closeButton;
+		private Image _icon;
+		private Grid _leftVerticalGrip;
+		private Button _minimiseButton;
 
-        private bool _mouseDown;
-        private Point _mouseDownPosition;
-        private Button _restoreButton;
-        private Grid _rightVerticalGrip;
+		private bool _mouseDown;
+		private Point _mouseDownPosition;
+		private Button _restoreButton;
+		private Grid _rightVerticalGrip;
 
-        private Grid _titleBar;
-        private Grid _topHorizontalGrip;
-        private Grid _topLeftGrip;
-        private Grid _topRightGrip;
+		private Grid _titleBar;
+		private Grid _topHorizontalGrip;
+		private Grid _topLeftGrip;
+		private Grid _topRightGrip;
 
-        public Control TitleBarContent
-        {
-            get { return GetValue(TitleBarContentProperty); }
-            set { SetValue(TitleBarContentProperty, value); }
-        }
+		public Control TitleBarContent
+		{
+			get { return GetValue(TitleBarContentProperty); }
+			set { SetValue(TitleBarContentProperty, value); }
+		}
 
-        Type IStyleable.StyleKey => typeof(MetroWindow);
+		Type IStyleable.StyleKey => typeof(MetroWindow);
 
-        protected override void OnPointerPressed(PointerPressedEventArgs e)
-        {
-            if (_topHorizontalGrip.IsPointerOver)
-            {
-                BeginResizeDrag(WindowEdge.North);
-            }
-            else if (_bottomHorizontalGrip.IsPointerOver)
-            {
-                BeginResizeDrag(WindowEdge.South);
-            }
-            else if (_leftVerticalGrip.IsPointerOver)
-            {
-                BeginResizeDrag(WindowEdge.West);
-            }
-            else if (_rightVerticalGrip.IsPointerOver)
-            {
-                BeginResizeDrag(WindowEdge.East);
-            }
-            else if (_topLeftGrip.IsPointerOver)
-            {
-                BeginResizeDrag(WindowEdge.NorthWest);
-            }
-            else if (_bottomLeftGrip.IsPointerOver)
-            {
-                BeginResizeDrag(WindowEdge.SouthWest);
-            }
-            else if (_topRightGrip.IsPointerOver)
-            {
-                BeginResizeDrag(WindowEdge.NorthEast);
-            }
-            else if (_bottomRightGrip.IsPointerOver)
-            {
-                BeginResizeDrag(WindowEdge.SouthEast);
-            }
-            else if (_titleBar.IsPointerOver)
-            {
+		protected override void OnPointerPressed(PointerPressedEventArgs e)
+		{
+			if (_topHorizontalGrip.IsPointerOver)
+			{
+				BeginResizeDrag(WindowEdge.North);
+			}
+			else if (_bottomHorizontalGrip.IsPointerOver)
+			{
+				BeginResizeDrag(WindowEdge.South);
+			}
+			else if (_leftVerticalGrip.IsPointerOver)
+			{
+				BeginResizeDrag(WindowEdge.West);
+			}
+			else if (_rightVerticalGrip.IsPointerOver)
+			{
+				BeginResizeDrag(WindowEdge.East);
+			}
+			else if (_topLeftGrip.IsPointerOver)
+			{
+				BeginResizeDrag(WindowEdge.NorthWest);
+			}
+			else if (_bottomLeftGrip.IsPointerOver)
+			{
+				BeginResizeDrag(WindowEdge.SouthWest);
+			}
+			else if (_topRightGrip.IsPointerOver)
+			{
+				BeginResizeDrag(WindowEdge.NorthEast);
+			}
+			else if (_bottomRightGrip.IsPointerOver)
+			{
+				BeginResizeDrag(WindowEdge.SouthEast);
+			}
+			else if (_titleBar.IsPointerOver)
+			{
 				_mouseDown = true;
 				_mouseDownPosition = e.GetPosition(this);
-            }
-            else
-            {
+			}
+			else
+			{
 				_mouseDown = false;
-            }
+			}
 
-            base.OnPointerPressed(e);
-        }
+			base.OnPointerPressed(e);
+		}
 
-        protected override void OnPointerReleased(PointerReleasedEventArgs e)
-        {
+		protected override void OnPointerReleased(PointerReleasedEventArgs e)
+		{
 			_mouseDown = false;
-            base.OnPointerReleased(e);
-        }
+			base.OnPointerReleased(e);
+		}
 
-        protected override void OnPointerMoved(PointerEventArgs e)
-        {
-            if ((_titleBar.IsPointerOver || _topHorizontalGrip.IsPointerOver) && _mouseDown)
-            {
-                if (_mouseDownPosition.DistanceTo(e.GetPosition(this)) > 2)
-                {
-                    WindowState = WindowState.Normal;
-                    BeginMoveDrag();
+		protected override void OnPointerMoved(PointerEventArgs e)
+		{
+			if ((_titleBar.IsPointerOver || _topHorizontalGrip.IsPointerOver) && _mouseDown)
+			{
+				if (_mouseDownPosition.DistanceTo(e.GetPosition(this)) > 2)
+				{
+					WindowState = WindowState.Normal;
+					BeginMoveDrag();
 					_mouseDown = false;
-                }
-            }
+				}
+			}
 
-            base.OnPointerMoved(e);
-        }
+			base.OnPointerMoved(e);
+		}
 
-        private void ToggleWindowState()
-        {
-            switch (WindowState)
-            {
-                case WindowState.Maximized:
-                    WindowState = WindowState.Normal;
-                    ToolTip.SetTip(_restoreButton, "Maximize");
-                    break;
+		private void ToggleWindowState()
+		{
+			switch (WindowState)
+			{
+				case WindowState.Maximized:
+					WindowState = WindowState.Normal;
+					ToolTip.SetTip(_restoreButton, "Maximize");
+					break;
 
-                case WindowState.Normal:
-                    WindowState = WindowState.Maximized;
-                    ToolTip.SetTip(_restoreButton, "Restore");
-                    break;
-            }
-        }
+				case WindowState.Normal:
+					WindowState = WindowState.Maximized;
+					ToolTip.SetTip(_restoreButton, "Restore Down");
+					break;
+			}
+		}
 
-        protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
-        {
+		protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
+		{
 			_titleBar = e.NameScope.Find<Grid>("titlebar");
 			_minimiseButton = e.NameScope.Find<Button>("minimiseButton");
 			_restoreButton = e.NameScope.Find<Button>("restoreButton");
@@ -176,5 +176,5 @@ namespace AvalonStudio.Shell.Controls
 				BorderThickness = new Thickness();
 			}
 		}
-    }
+	}
 }
